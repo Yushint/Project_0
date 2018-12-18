6fffb77747a51a7828e040905c862324b13b6e55
Yushint <45449706+Yushint@users.noreply.github.com>
2018-12-18 22:42:10 +0300

    Add dialoge and save buttons. Also something else...
    Release Version!

--- MainTablitsa.py
+++ MainTablitsa.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget, QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget, QTableWidget, QTableWidgetItem, QVBoxLayout, QLabel, QLineEdit
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtWidgets import QPushButton
 
@@ -13,19 +13,43 @@ class MainWindow(QMainWindow):
         self.initUI()
         
     def initUI(self):
-        self.setGeometry(100,100,100,100)
+        self.start_layout = QVBoxLayout()
+        
+        self.label = QLabel(self)
+        self.label.setText("Приветствую, ...")
+        self.label.move(10,0)
+    
+        
+        self.name_label = QLabel(self)
+        self.name_label.setText("Введите имя: ")
+        self.name_label.move(10,13)
+        
+        self.name_input = QLineEdit(self)
+        self.name_input.move(10,35)
+        
+        self.btn0 = QPushButton('сохранить', self)
+        self.btn0.move(110,80)
+        self.btn0.resize(self.btn0.sizeHint())
+        self.btn0.clicked.connect(self.hello)
+        
+        self.setGeometry(300,300,300,300)
         self.setWindowTitle("Таблица Умножения") # заголовок окна
         
         self.button_1 = QPushButton(self)
-        self.button_1.move(50,50)
-        self.button_1.setText("Запустить?")
+        self.button_1.move(110,50)
+        self.button_1.setText("Запуск")
         self.button_1.clicked.connect(self.run)
         
         self.button_2 = QPushButton(self)
-        self.button_2.move(50,20)
+        self.button_2.move(110,20)
         self.button_2.setText("Выход")
         self.button_2.clicked.connect(self.run_2)
         self.show()
+        
+    def hello(self):
+        self.name = self.name_input.text()
+        self.label.setText("Привет, {}".format(self.name))
+    
     def run(self):
         self.setMinimumSize(QSize(5,5))     #  мин.размеры
         central_widget = QWidget(self)                  # создаем центральный виджет
